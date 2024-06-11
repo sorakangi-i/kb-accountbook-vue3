cd019d0fa198543c7d084534b43954287aebd787
Sim yun jae <sim232811@naver.com>
2024-06-11 11:47:10 +0900

    simpson-06-11

--- kb-accountbook-vue3/src/App.vue
+++ kb-accountbook-vue3/src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Home</router-link>
+      </div>
     </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
+<script setup></script>
 
 <style>
-/* 스타일 정의 */
-
+@import 'bootstrap/dist/css/bootstrap.min.css';
+</style>
