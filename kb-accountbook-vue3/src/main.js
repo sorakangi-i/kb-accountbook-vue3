c17168d4a26d4b8bf116aafcff81fdeee0e2b966
chaeyoung <chyoung000@naver.com>
2024-06-11 11:27:53 +0900

    chaeyoung

--- kb-accountbook-vue3/src/main.js
+++ kb-accountbook-vue3/src/main.js
@@ -1,4 +1,4 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -6,9 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+import 'bootstrap/dist/css/bootstrap.css'
 
 app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
