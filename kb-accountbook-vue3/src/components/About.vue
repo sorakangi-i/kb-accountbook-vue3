1c5e7c014b6082a2ddae2295ef222de56a222296
Sim yun jae <sim232811@naver.com>
2024-06-11 11:04:43 +0900

    simpson-06-11

---
+++ kb-accountbook-vue3/src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <p>This is the About Page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+};
+</script>
+
+<style scoped>
+/* 스타일 정의 */
+</style>
