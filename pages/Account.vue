ee7d8472c3d91318400d6cc6fa5e83771516c017
chaeyoung <chyoung000@naver.com>
2024-06-12 14:29:37 +0900

    2024-06-11 진짜

---
+++ pages/Account.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="account">
+    <h1>Account Information</h1>
+    <Table />
+  </div>
+</template>
+
+<script>
+import Table from './components/Table.vue';
+
+export default {
+  components: {
+    Table
+  }
+}
+</script>
+
+<style scoped>
+.account {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh; /* 전체 화면 높이 */
+}
+
+h1 {
+  font-size: 24px;
+  margin-bottom: 20px;
+  text-align: center;
+}
+</style>
