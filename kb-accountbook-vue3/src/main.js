cd110fd86e4671b462eb127b5dc98d25299b07be
Sim yun jae <sim232811@naver.com>
2024-06-13 23:05:13 +0900

    simspon

--- kb-accountbook-vue3/src/main.js
+++ kb-accountbook-vue3/src/main.js
@@ -8,6 +8,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import '@fortawesome/fontawesome-free/css/all.css'; // 이 줄을 추가하세요.
+
 library.add(fas);
 
 const app = createApp(App);
